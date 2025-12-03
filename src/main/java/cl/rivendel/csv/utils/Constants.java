942fa5ea9c1d1e2b777874e8e06f32487a9c679c
Sebastián Eduardo Pavez Morales <edkeats@Sebastians-MacBook-Pro.local>
2025-12-03 13:50:13 -0300

    Constants

--- src/main/java/cl/rivendel/csv/utils/Constants.java
+++ src/main/java/cl/rivendel/csv/utils/Constants.java
@@ -1,10 +1,12 @@
 package cl.rivendel.csv.utils;
 
+import java.io.File;
+
 public class Constants {
     private Constants(){
         throw new UnsupportedOperationException("Util class");
     }
-    public static final String ORACLE_CARDS_FOLDER = ".\\oracleCards\\";
+    public static final String ORACLE_CARDS_FOLDER = "." + File.separator + "oracleCards" + File.separator;
     public static final String ORACLE_CARDS = "oracle_cards";
     public static final String ALL_CARDS = "all_cards";
     public static final String HIGH_RES_SCAN = "highres_scan";
